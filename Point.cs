88a945a7aa1e09d193ea1fbf75e1fb286aa1d169
askrivenko <ppkrivenko@gmail.com>
2017-03-18 21:36:17 +0300

    Class

---
+++ Point.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication_Snake
+{
+	class Point
+	{
+		public int x;
+		public int y;
+		public char symbol;
+
+		public void Draw()
+		{
+			Console.SetCursorPosition(x, y);
+			Console.Write(symbol);
+		}
+
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication_Snake
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+
+			Point p = new Point { x = 10, y = 10, symbol = '*' };
+			p.Draw();
+			Point p2 = new Point { x = 100, y = 10, symbol = '*' };
+			p2.Draw();
+
+			Console.ReadKey();
+		}
+
+	}
+}
